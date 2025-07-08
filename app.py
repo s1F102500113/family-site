1c3dc798ae03513d8ddee1d82bbb9ea99a6919e2
s1F102500113 <s1F102500113@iniad.org>
2025-07-09 00:47:14 +0900

    id以外修正

--- app.py
+++ app.py
@@ -6,33 +6,33 @@ import os, random
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
-message = []
+info = {}
 
 def randomid(n):
    randlst = [random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for i in range(n)]
    return ''.join(randlst)
 id = randomid(10)
 
-current_time = datetime.now().isoformat(sep='-', timespec='seconds')
+
 
 
 
 @app.route('/')
 def top():
     global message
-    
+    currenttime = (datetime.now().isoformat(sep='-', timespec='seconds'))
+
     new_message = session.pop('text', None)
-
     if new_message:
-        message.append(new_message)
+        info[new_message] = currenttime
     
 
-    return render_template('famCH.html', message=message, id=id, current_time=current_time)
+    return render_template('famCH.html', id=id, info=info)
 
 
 @app.route("/famCH", methods=["POST"])
 def famCH():
-
+    
     session['text'] = request.form.get('text')
     
     return redirect(url_for('top'))
